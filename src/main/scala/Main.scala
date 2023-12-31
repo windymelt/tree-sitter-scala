8c3f2792355f831271590046d34469c53d3c2e78
Windymelt <windymelt@3qe.us>
2023-12-31 19:40:41 +0900

    removed unused code

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,5 +1,5 @@
-import scala.scalanative.libc.string.strlen
 import treesitter.all.*
+import scalanative.libc.string.strlen
 import scalanative.unsafe.*
 import scalanative.unsigned.UnsignedRichInt
 
@@ -13,29 +13,5 @@ object Main {
     val source = c"""{"foo":42, "bar": "windymelt"}"""
     val tree = ts_parser_parse_string(parser, null, source, strlen(source).toUInt)
     ts_tree_print_dot_graph(tree, 1 /* stdout*/)
-    val rootNode = ts_tree_root_node(tree)
-    // printChildren(rootNode)
   }
-
-  // from https://github.com/indoorvivants/sn-bindgen-examples
-  def printChildren(start: TSNode)(implicit zone: Zone): Unit =
-    def go(node: TSNode, level: Int): Unit =
-      val nodeType = fromCString(ts_node_type(node))
-      val startByte = ts_node_start_byte(node)
-      val endByte = ts_node_end_byte(node)
-      print(" " * level)
-      println(s"${nodeType}:${startByte}:${endByte}")
-      // asInstanceOf is a bug in codegen
-      val childrenCount = ts_node_child_count(node)
-      if (childrenCount != 0.toUInt) {
-        for {childId <- 0 until childrenCount.toInt} do {
-          val childNode =
-            ts_node_child(node, childId.toUInt)
-          go(childNode, level + 1)
-        }
-      }
-    end go
-
-    go(start, 0)
-  end printChildren
 }
